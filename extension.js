f1663ee4a1f615df4279d3ac6433bd9d221c3739
Huacai Sang <sang@miro.com>
2024-01-09 19:37:16 +0100

    WIP

--- extension.js
+++ extension.js
@@ -5,15 +5,6 @@ const editor = require('./editor/editor');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('disorder-editor.hey', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('How are you!');
-	});
-
-	context.subscriptions.push(disposable);
-
 	context.subscriptions.push(editor.EditorProvider.register(context));
 }
 

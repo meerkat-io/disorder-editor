06b971e3afb9a0f037424eae055fdec33d0db162
Huacai Sang <sang@miro.com>
2024-09-02 23:01:03 +0200

    WIP

--- webview-src/App.vue
+++ webview-src/App.vue
@@ -4,7 +4,7 @@ import { ref, onMounted } from 'vue'
 import Schema from './components/Schema.vue'
 import Message from './components/Message.vue'
 import Cell from './components/Cell.vue'
-import { OutMessageType, InMessageType, Edit } from './shared'
+import { OutMessageType, InMessageType, Edit, OperationType } from './shared'
 
 // @ts-ignore
 const vscode = acquireVsCodeApi();
@@ -15,9 +15,17 @@ const datagrid = ref();
 const view = ref(0);
 
 /**
- * @property {Edit[]} edits
- * @property {Edit[]} savedEdits
+ * @type {Edit[]}
  */
+const edits = [];
+/**
+ * @type {Edit[]}
+ */
+const savedEdits = [];
+/**
+ * @type {Edit[]}
+ */
+const redoEdits = [];
 
 const View = {
     NONE: 0,
@@ -48,20 +56,56 @@ function receiveMessage(message) {
             break;
 
         case InMessageType.UNDO:
+            console.log('undo');
+            const undo = edits.pop();
+            redoEdits.push(undo);
+            setValue(undo.undo.path, undo.undo.value);
             break;
 
         case InMessageType.REDO:
+            console.log('redo');
+            const redo = redoEdits.pop();
+            edits.push(redo);
+            setValue(redo.redo.path, redo.redo.value);
             break;
     }
+}
+
+/**
+ * @param {string} path
+ * @param {any} value
+ */
+function setValue(path, value) {
+    let obj = datagrid.value.value;
+    const parts = path.split('.');
+    const lastPart = parts[parts.length - 1];
+    for (let i = 0; i < parts.length - 1; i++) {
+        obj = obj[parts[i]];
+    }
+    obj[lastPart] = value;
 }
 
 /**
  * @param {Edit} edit
  */
 function handleEdit(edit) {
-    console.log('send edit in App ==============');
-    console.log(edit);
-    vscode.postMessage({ command: OutMessageType.EDIT, body: edit });
+    let merged = false;
+    if (edits.length > 0) {
+        const lastEdit = edits[edits.length - 1];
+        if (lastEdit.undo.path == edit.undo.path
+            && lastEdit.undo.type == OperationType.UPDATE
+            && edit.undo.type == OperationType.UPDATE) {
+            lastEdit.redo.value = edit.redo.value;
+            merged = true;
+        }
+    }
+    if (redoEdits.length > 0) {
+        redoEdits.splice(0, redoEdits.length);
+    }
+    if (!merged) {
+        edits.push(edit);
+        vscode.postMessage({ command: OutMessageType.EDIT });
+    }
 }
 
 vscode.postMessage({ command: OutMessageType.READY });
@@ -69,42 +113,44 @@ vscode.postMessage({ command: OutMessageType.READY });
 
 /**
  * 
-	load() {
-		
-	}
+    load() {
+    	
+    }
 
-	save(cancellation) {
-		this.saveAs(this.uri, cancellation);
-		this.savedEdits = Array.from(this.edits);
-	}
+    save(cancellation) {
+        this.saveAs(this.uri, cancellation);
+        this.savedEdits = Array.from(this.edits);
+    }
 
-	saveAs(targetResource, cancellation) {
-		//TODO: debug
-		return;
-		this.uri = targetResource;
-		if (cancellation.isCancellationRequested) {
-			return;
-		}
-		this.file.filePath = this.uri.path;
-		this.file.write(this.file.value);
-	}
+    saveAs(targetResource, cancellation) {
+        //TODO: debug
+        return;
+        this.uri = targetResource;
+        if (cancellation.isCancellationRequested) {
+            return;
+        }
+        this.file.filePath = this.uri.path;
+        this.file.write(this.file.value);
+    }
 
-	revert(_cancellation) {
-		this.load();
-		this.edits = this.savedEdits;
+    revert(_cancellation) {
+        this.load();
+        this.edits = this.savedEdits;
 
-		this.onDidChangeDocument.fire({
-			content: fs.readFileSync(this.uri.fsPath),
-			edits: this.edits,
-		});
-	}
+        this.onDidChangeDocument.fire({
+            content: fs.readFileSync(this.uri.fsPath),
+            edits: this.edits,
+        });
+    }
  */
 </script>
 
 <template>
     <schema v-if="view == View.SCHEMA"
-        @select="(schemaPath) => vscode.postMessage({ command: OutMessageType.SCHEMA, body: schemaPath })" :status="schema" />
+        @select="(schemaPath) => vscode.postMessage({ command: OutMessageType.SCHEMA, body: schemaPath })"
+        :status="schema" />
     <message v-if="view == View.MESSAGE"
-        @select="(message) => vscode.postMessage({ command: OutMessageType.MESSAGE, body: message })" :messages="messages" />
+        @select="(message) => vscode.postMessage({ command: OutMessageType.MESSAGE, body: message })"
+        :messages="messages" />
     <cell v-else-if="view == View.DATA" :type="datagrid.type" v-model="datagrid.value" :path="''" @edit="handleEdit" />
 </template>
